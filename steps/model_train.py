9ed7f6eb2331989e213161c38e356b08f21383e7
ak <binod@gmail.com>
2024-04-11 23:59:11 +0530

    model training file updated

--- steps/model_train.py
+++ steps/model_train.py
@@ -1,13 +1,35 @@
 import logging
 import pandas as pd
 from zenml import step
+from src.model_dev import LinearRegressionModel
+from sklearn.base import RegressorMixin
+from .config import ModelNameConfig
 
 @step
-def train_model(df:pd.DataFrame)->None:
+def train_model(
+    X_train:pd.DataFrame,
+    X_test : pd.DataFrame,
+    y_train : pd.Series,
+    y_test : pd.Series,
+    config : ModelNameConfig
+) -> RegressorMixin:
     """
     Trains the model on the cleansed ingested data.
     
     Args:
-        df : the ingested data
+            X_train:pd.DataFrame,
+            X_test : pd.DataFrame,
+            y_train : pd.Series,
+            y_test : pd.Series
     """
-    pass
+    try:
+        model = None
+        if config.model_name  == "LinearRegression":
+            model = LinearRegressionModel()
+            trained_model = LinearRegressionModel().train(X_train,y_train)
+            return train_model
+        else:
+            raise ValueError("Model {} not supported".format(config.model_name))
+    except Exception as e:
+        logging.error("Error in training model : {}".format(e))
+        raise e
