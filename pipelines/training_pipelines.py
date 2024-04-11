85e6f35b2f479828a2c117cfb8732130e61e5ee5
ak <binod@gmail.com>
2024-04-12 00:24:19 +0530

    updated training_pipelines.py file

--- pipelines/training_pipelines.py
+++ pipelines/training_pipelines.py
@@ -8,6 +8,7 @@ from steps.evaluation import evaluate_model
 @pipeline(enable_cache=True)
 def train_pipeline(data_path:str):
     df = ingest_df(data_path)
-    clean_df(df)
-    train_model(df)
-    evaluate_model(df)
+    X_train, X_test, y_train, y_test = clean_df(df)
+
+    model = train_model(X_train, X_test, y_train, y_test) 
+    mse, rmse, r2 = evaluate_model(model, X_test, y_test)
